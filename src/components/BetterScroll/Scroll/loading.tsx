9331903c3fcaffcf5d400ba481c4efcb3c7f0624
TothingWay <200846290@qq.com>
2020-07-20 17:57:52 +0800

    feat: add scroll loading animation

---
+++ src/components/BetterScroll/Scroll/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './index.module.scss'
+
+function Loading() {
+  return (
+    <div className={style['loading']}></div>
+  )
+}
+
+export default Loading
